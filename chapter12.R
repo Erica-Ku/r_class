4eb05843bc017b1f900bef358e6504966adbdfe1
Erica-Ku <yeskuminji@naver.com>
2023-03-28 13:09:54 +0900

    (0328)

---
+++ chapter12.R
@@ -0,0 +1,44 @@
+# 12장 연습문제1
+install.packages("gmodels")
+library(gmodels)
+
+setwd("c:/Rwork/Part-III")
+smoke <- read.csv("./smoke.csv", header = TRUE)
+head(smoke)
+
+smoke$education2[smoke$education==1] <- "1. 대졸"
+smoke$education2[smoke$education==2] <- "2. 고졸"
+smoke$education2[smoke$education==3] <- "3. 중졸"
+smoke$smoking2[smoke$smoking==1] <- "1. 과대흡연"
+smoke$smoking2[smoke$smoking==2] <- "2. 보통흡연"
+smoke$smoking2[smoke$smoking==3] <- "3. 비흡연"
+
+table(smoke$education2, smoke$smoking2)
+
+CrossTable(smoke$education2, smoke$smoking2, chisq = T)
+
+# 12장 연습문제2
+data <- read.csv("./cleanData.csv", fileEncoding = "euc-kr")
+str(data)
+
+x <- data$position
+y <- data$age3
+
+plot(x,y, abline(lm(y~x)))
+
+CrossTable(x,y,chisq = T)
+
+# 12장 연습문제3
+result <- read.csv("response.csv", header = TRUE)
+str(result)
+
+result$job2[result$job==1] <- "1. 학생"
+result$job2[result$job==2] <- "2. 직장인"
+result$job2[result$job==3] <- "3. 주부"
+result$response2[result$response==1] <- "1. 무응답"
+result$response2[result$response==2] <- "2. 낮음"
+result$response2[result$response==3] <- "3. 높음"
+
+table(result$job2, result$response2)
+
+chisq.test(result$job2, result$response2)
